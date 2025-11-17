d954a3af6726544fb2942966543bc6fa40609f94
Sushovan Ghosh <sushovan1908@gmail.com>
2025-11-17 22:57:50 +0530

    Done with design started animation so can say started a bit of js

---
+++ index.js
@@ -0,0 +1,13 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+gsap.from(".nlink",{
+    stagger:.1,
+    y:10,
+    duration:2,
+    power: escape3,
+    opacity:0
+
+})
